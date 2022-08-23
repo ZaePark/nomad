a2b316415392bba5d16fedc415de11f217d7dd80
SJ-P4RK <tom9411@naver.com>
2022-08-23 20:50:20 +0900

    update canvas

--- meme_maker/app.js
+++ meme_maker/app.js
@@ -1,1 +1,42 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = 2;
+
+function onclick(event){
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke()
+}
+
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.fill();
+
+
+
+// function onClick(event) {
+//     ctx.beginPath();
+//     ctx.moveTo(800, 800);
+//     const color = colors[Math.floor(Math.random() * colors.length)];
+//     ctx.strokeStyle = color;
+//     ctx.lineTo(event.offsetX, event.offsetY);
+//     ctx.stroke();
+//   }
+
+// const colors = [
+//   "#ff3838",
+//   "#ffb8b8",
+//   "#c56cf0",
+//   "#ff9f1a",
+//   "#fff200",
+//   "#32ff7e",
+//   "#7efff5",
+//   "#18dcff",
+//   "#7d5fff",
+// ];
+
+// canvas.addEventListener("mousemove", onClick);
+
